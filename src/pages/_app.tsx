17a53126bf8abdc4cc2849a08264d3fea9253bca
Fitsum Mekonnen <official.fitsum.mekonnen@gmail.com>
2023-05-20 00:19:05 +0300

    Next Image tag, style up

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,19 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-
+import { dark } from "@clerk/themes";
+import type { BaseThemeTaggedType } from "@clerk/types";
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ClerkProvider {...pageProps}>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark as BaseThemeTaggedType,
+        variables: {
+          colorPrimary: "#e2e8f0",
+        },
+      }}
+      {...pageProps}
+    >
       <Component {...pageProps} />
     </ClerkProvider>
   );
